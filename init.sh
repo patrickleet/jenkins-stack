3eb2c469999f04e11c61bf960481181b3a0ca857
Patrick Scott <patrick.scott@zx-ventures.com>
2017-09-18 16:13:49 -0400

    update for d4aws

--- init.sh
+++ init.sh
@@ -2,3 +2,7 @@ echo "$JENKINS_USER" | docker secret create jenkins-user -
 echo "$JENKINS_PASS" | docker secret create jenkins-pass -
 
 docker network create --driver overlay jenkins
+
+export SLACK_IP=$(sudo ping \
+  -c 1 $SLACK_DOMAIN \
+  | awk -F'[()]' '/PING/{print $2}')
