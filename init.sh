06a82a9f69667fce996edc19b48cd87e936fcb5e
Patrick Scott <patrick.scott@zx-ventures.com>
2017-09-18 17:30:02 -0400

    update jenkins stack

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ echo "$JENKINS_PASS" | docker secret create jenkins-pass -
 docker network create --driver overlay jenkins
 
 export SLACK_IP=$(sudo ping \
-  -c 1 $SLACK_DOMAIN \
+  -c 1 $SLACK_SUBDOMAIN.slack.com \
   | awk -F'[()]' '/PING/{print $2}')
